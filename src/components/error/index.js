1402e860e0d4658172e06b8bcd9f622efa2ed023
Jaroslav-Lesiv <jaroslav_lesiv@outlook.com>
2017-11-15 19:05:39 +0200

    refactoring

---
+++ src/components/error/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ErrorBlock extends Component {
+    render() {
+        return (
+            <div>Error Block</div>
+        )
+    }
+}
+export default ErrorBlock
